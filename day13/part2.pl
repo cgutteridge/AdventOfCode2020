1278d2b3d8d2ff7458503b770fc581d0b07df457
totl <totl@soton.ac.uk>
2020-12-13 11:53:00 +0000

    notes

--- day13/part2.pl
+++ day13/part2.pl
@@ -20,6 +20,7 @@ for( my $i=0; $i<@list; ++$i ) {
 # find the number of iteraions between two buses to get the right offset, then the number of interations of that to get the next
 my $start = 0;
 my $loop_size = 1;;
+my $x=0;
 BUS: foreach my $bus ( @buses ) {
 	my $offset = $offs->{$bus};
 	if( !defined $loop_size ) {
@@ -32,21 +33,27 @@ BUS: foreach my $bus ( @buses ) {
 	# how many times do we have to do this loop so that bus $bus is $offset minutes ahead?
 	my $loops = 0;
 	while(1) {
+		# starting at the offset right for all the buses previous to now,
+		#  try offsets from that that increment by the loop size (the product of all previous buses) 
+		#  the correct answer is when (that time + new bus required offset) % new bus loopsize == 0
 		my $time = $start + $loops * $loop_size;
 		# does the new bus leave $offset minutes after this?
-		my $l = ($time+$offset) / $bus;
 	#print "bus=$bus time=$time l=$l \n";
-		if( $l == int $l ) {
+		if( ($time+$offset) % $bus == 0 ) {
 			# yes (it's an interger number of times)
 			# to get to the same state for the other buses we have to do a loop the size of the sum of their primes. 
-			# and as they are all prime, it's just their sum
+			# and as they are all prime, it's just their sum, so repeat process for next bus starting at this time using
+			# the loop size of how often this state will recur (the new loop_size)
 			$loop_size = $loop_size*$bus;
 			$start = $time;
+			#print "$loops\n";
 			next BUS;
 		}
 		$loops++;
+		$x++;
 	}
 }
+print "total loops = $x\n";
 print sprintf( "PART2 = %d\n", $start );
 exit;
 
