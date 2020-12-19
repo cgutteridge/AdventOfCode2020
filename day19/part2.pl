36bdcba15e8295f99551f1132547c7fa0e3bd3f6
totl <totl@soton.ac.uk>
2020-12-19 13:40:44 +0000

    done

--- day19/part2.pl
+++ day19/part2.pl
@@ -8,7 +8,7 @@ my $rules = {};
 my $lists = {};
 
 my @messages = ();
-my @rows = readfile( "ex2" );
+my @rows = readfile( "data" );
 my $i=0;
 while( $rows[$i] ne "" ) {
 	my( $n, $rule ) = split( ": ", $rows[$i] );
@@ -34,27 +34,6 @@ while( $i<scalar @rows ) {
 print "MAX MESSAGE $maxm\n";
 
 #8: 42*
-#11: 42 31 | 42 42 31 31 | 42 42 42 31 31 31  etc.
-# create fake rules to resolve these when 42 & 31 are known
-$rules->{8}=[
-	[42],
-	[42,42],
-	[42,42,42],
-	[42,42,42,42],
-	[42,42,42,42,42],
-	[42,42,42,42,42,42],
-	[42,42,42,42,42,42,42],
-	[42,42,42,42,42,42,42,42],
-	[42,42,42,42,42,42,42,42,42],
-	[42,42,42,42,42,42,42,42,42,42],
-];
-$rules->{11}=[
-[42,31],
-[42,42,31,31],
-[42,42,42,31,31,31],
-[42,42,42,42,31,31,31,31],
-[42,42,42,42,42,31,31,31,31,31],
-];
 
 # v1 try to collapse rules
 my @remaining_rules = keys %$rules;
@@ -109,16 +88,53 @@ print "Depth:$D\n";
 	}	
 	#if( $rnum == 42 ) { print Dumper( $lists->{$rnum} ); }
 }
-#print Dumper( $lists );
+foreach my $i42 ( keys %{$lists->{42}} ) {
+	if( defined $lists->{31}->{$i42} ) {
+		die "dang $i42";
+	}
+}
+# ok so 42 & 31 don't intersect
+print Dumper( $lists->{42}, $lists->{31} );
+
 print "--\n";
 my $matches = 0;
-foreach my $mess ( @messages ) {
+MESS: foreach my $mess ( @messages ) {
 	print $mess . " :";
-	if( $lists->{0}->{$mess} ) {
-		print " MATCH";
-		$matches++;
+	print "(".length( $mess).") ";
+	if( $lists->{0}->{$mess} ) { print " OLDMATCH"; }
+	my @chunks = ();
+	for(my $i=0;$i<length($mess);$i+=8 ) {
+		push @chunks, substr( $mess,$i,8);
 	}
-	print "\n";
+
+	# need 2+X then 1+Y and X must be bigger or equal to Y+1 (minimum is XXY)
+	my $r = "";
+	my $X=0; #42
+	my $Y=0; #31
+	foreach my $c ( @chunks ) {
+		# if we get a X(42) after and Y(31) then it's invalid
+		if( $lists->{42}->{$c} ) { $r .= "X"; $X++; if( $Y>0) { print "- FAIL Y BEFORE X -- $r\n"; next MESS; } }
+		elsif( $lists->{31}->{$c} ) { $r .= "Y" ; $Y++; }
+		else { next MESS; }
+	}
+
+	# needs to match an 8 then an 11
+	# which means 42* followed by x 42s then  x31s
+
+	# aka
+	# any 1+ of 42 then 1+ of 31 but not more 31 than 42.
+
+	print " - $r";
+	if( $X<$Y+1 ) {
+		print " Failed $X<$Y+1\n";
+		next MESS ;
+	}
+	if( $Y == 0 ) {
+		print " Failed $Y is 0\n";
+		next MESS ;
+	}
+	$matches++;
+	print " - MATCH\n";
 }
 print sprintf( "PART2 = %d\n", $matches );
 exit;
